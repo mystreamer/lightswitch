e3c837a4367d7beae012d8e51c8fdd9929738ae5
Dylan <me@dylanmassey.ch>
2021-08-25 19:01:54 +0200

    Added translation function

--- test/utils_test.py
+++ test/utils_test.py
@@ -1,12 +1,21 @@
 import unittest
 from utils.utils import CTFIDFVectorizer
+from utils.utils import Translator
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
 
 class CTFIDFVectorizerTest(unittest.TestCase):
 	def setUp(self):
 		self.text = ["This is a test on this system", "This is not a test just random", "Here is another test sentence"]
 		self.count_matrix = CountVectorizer().fit_transform(self.text)
+
 	def test_csr_output(self):
 		ctfidf = CTFIDFVectorizer().fit_transform(self.count_matrix, n_samples = len(self.text)).toarray()
 		assert ctfidf.shape == (3,11)
 
+
+class TranslatorTest(unittest.TestCase):
+	def test_translation(self):
+		t = Translator(auth_key="c1f62eb8-649b-514f-1f73-b3dc19e1c339:fx", source_lang="EN", target_lang="DE")
+		ttext = t.translate_text("Hello, world!")
+		assert ttext == "Hallo, Welt!"
--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,11 @@
 # essentially wrappers for classes / functions that require a single call
+import json
+import requests
 import numpy as np
 import scipy.sparse as sp
 from sklearn.preprocessing import normalize
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+
 
 class CTFIDFVectorizer(TfidfTransformer):
     def __init__(self, *args, **kwargs):
@@ -24,3 +27,29 @@ class CTFIDFVectorizer(TfidfTransformer):
             X = X * self._idf_diag
             X = normalize(X, axis=1, norm='l1', copy=False)
             return X
+
+
+class Translator(object):
+    api_root = "https://api-free.deepl.com/v2/translate"
+
+    def __init__(self, auth_key, source_lang, target_lang):
+        self.auth_key = auth_key
+        self.source_lang = source_lang
+        self.target_lang = target_lang
+
+    def translate_text(self, text):
+        params = {
+            "auth_key": self.auth_key,
+            "source_lang": self.source_lang,
+            "text": text,
+            "target_lang": self.target_lang
+        }
+
+        try:
+            res = requests.get(f"https://api-free.deepl.com/v2/translate", params=params)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+        # TODO: Cases for multiple translations, if even possible by the API.
+
+        return res.json()["translations"][0]["text"]
