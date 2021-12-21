e8674889dd56453da64023551c29d2a3fcfaf76f
Francesco <33670163+Fenerator@users.noreply.github.com>
2021-12-21 19:04:28 +0100

    update preprocessing

--- misc/preprocessing.py
+++ misc/preprocessing.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from os import read
 #from parse_html import standardize_dates
 import spacy
@@ -7,11 +8,16 @@ import numpy as np
 import re
 import nltk
 from collections import Counter
-# from langdetect import detect
+from langdetect import detect
 from nltk.corpus import stopwords
 #import ssl
-# import dateparser
+import dateparser
 import logging
+import dill as pickle
+
+from joblib import Parallel, delayed
+from multiprocessing import cpu_count
+from math import ceil
 
 
 logging.basicConfig(filename='preprocessing.log', filemode='w', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
@@ -39,21 +45,13 @@ def to_lowercase(text_list):
     return articles
 
 
-def preprocess(text_list, language_list, pos_tags_to_keep, filter_list):
-    nlp_en = spacy.load("en_core_web_sm")
-    stop_words_en = set(stopwords.words('english')) # nltk stopword list
 
-    nlp_de = spacy.load('de_core_news_sm')
-    stop_words_de = set(stopwords.words('german'))
 
-    nlp_fr = spacy.load('fr_core_news_sm')
-    stop_words_fr = set(stopwords.words('french'))
 
-    nlp_it = spacy.load('it_core_news_sm')
-    stop_words_it = set(stopwords.words('italian'))
 
-    lang_modul = {'en': nlp_en, 'de': nlp_de, 'fr': nlp_fr, 'it': nlp_it}
-    stop_lang = {'en': stop_words_en, 'de': stop_words_de, 'fr': stop_words_fr, 'it': stop_words_it}
+
+def preprocess_helper(text_list, language_list, pos_tags_to_keep, filter_list, stop_lang, lang_modul):
+    
     counter = 1
     lemmatized_text = []
     for article, lang_id in zip(text_list, language_list):
@@ -73,11 +71,7 @@ def preprocess(text_list, language_list, pos_tags_to_keep, filter_list):
                     if token.text.lower() not in filter_list:
                         #print(f'Token text: {token.text} POS={token.pos_}') # TESTEN
                         if token.pos_ in pos_tags_to_keep:
-                            if token.pos_ == 'NUM':
-                                if len(token.text.lower()) == 4:
-                                    tokenized_article.append(token.lemma_.lower())
-                            else:
-                                tokenized_article.append(token.lemma_.lower()) # setting to lowercase
+                            tokenized_article.append(token.lemma_.lower()) # setting to lowercase
         
         lemmatized_text.append(' '.join(tokenized_article))
         print(f'Preprocessing Art. ID: {counter}')
@@ -88,43 +82,44 @@ def preprocess(text_list, language_list, pos_tags_to_keep, filter_list):
 
 def read_csv(overview_file):
     df = pd.read_csv(overview_file, encoding='utf-8')
-    # df.set_index('id', drop=False)
+    '''
+    df.set_index('id', drop=False)
 
-    # # Check if all columns exist:
-    # if 'sub_org' not in df.columns:
-    #     df['sub_org'] = np.nan
+    # Check if all columns exist:
+    if 'sub_org' not in df.columns:
+        df['sub_org'] = np.nan
 
-    # if 'title' not in df.columns:
-    #     df['title'] = np.nan
+    if 'title' not in df.columns:
+        df['title'] = np.nan
 
-    # if 'date' not in df.columns:
-    #     df['date'] = np.nan
+    if 'date' not in df.columns:
+        df['date'] = np.nan
 
-    # if 'teaser' not in df.columns:
-    #     df['teaser'] = np.nan
+    if 'teaser' not in df.columns:
+        df['teaser'] = np.nan
 
-    # if 'article_text' not in df.columns:
-    #     df['article_text'] = np.nan
+    if 'article_text' not in df.columns:
+        df['article_text'] = np.nan
 
-    # if 'date_standardized' not in df.columns:
-    #     df['date_standardized'] = np.nan
+    if 'date_standardized' not in df.columns:
+        df['date_standardized'] = np.nan
 
-    # if 'text_preprocessed' not in df.columns:
-    #     df['text_preprocessed'] = np.nan
+    if 'text_preprocessed' not in df.columns:
+        df['text_preprocessed'] = np.nan
 
-    # if 'language_id' not in df.columns:
-    #     df['language_id'] = np.nan
+    if 'language_id' not in df.columns:
+        df['language_id'] = np.nan
 
-    # # set type of columns
-    # df["sub_org"]=df["sub_org"].astype(str)
-    # df["title"]=df["title"].astype(str)
-    # df["date"]=df["date"].astype(str)
-    # df["teaser"]=df["teaser"].astype(str)
-    # df["article_text"]=df["article_text"].astype(str)
-    # df["date_standardized"]=df["date_standardized"].astype(str)
-    # df["text_preprocessed"]=df["text_preprocessed"].astype(str)
-    # df["language_id"]=df["language_id"].astype(str)
-
+    # set type of columns
+    df["sub_org"]=df["sub_org"].astype(str)
+    df["title"]=df["title"].astype(str)
+    df["date"]=df["date"].astype(str)
+    df["teaser"]=df["teaser"].astype(str)
+    df["article_text"]=df["article_text"].astype(str)
+    df["date_standardized"]=df["date_standardized"].astype(str)
+    df["text_preprocessed"]=df["text_preprocessed"].astype(str)
+    df["language_id"]=df["language_id"].astype(str)
+    '''
     return df
 
 
@@ -214,30 +209,62 @@ def standardize_dates(overview_file, df):
     
     return df
 
+def preprocess(foo_list, language_list, pos_tags_to_keep, filter_list, nr_workers = cpu_count()):
+    nlp_en = spacy.load("en_core_web_sm")
+    stop_words_en = set(stopwords.words('english')) # nltk stopword list
+
+    nlp_de = spacy.load('de_core_news_sm')
+    stop_words_de = set(stopwords.words('german'))
+
+    nlp_fr = spacy.load('fr_core_news_sm')
+    stop_words_fr = set(stopwords.words('french'))
+
+    nlp_it = spacy.load('it_core_news_sm')
+    stop_words_it = set(stopwords.words('italian'))
+
+    lang_modul = {'en': nlp_en, 'de': nlp_de, 'fr': nlp_fr, 'it': nlp_it}
+    stop_lang = {'en': stop_words_en, 'de': stop_words_de, 'fr': stop_words_fr, 'it': stop_words_it}
+
+    chunk_size = ceil(len(foo_list) / nr_workers)
+    chunks = [foo_list[x:x+chunk_size] for x in range(0, len(foo_list), chunk_size)]
+    results = Parallel(n_jobs=nr_workers, backend="multiprocessing")(delayed(preprocess_helper)(text_list=chunk, language_list=language_list, pos_tags_to_keep=pos_tags_to_keep, filter_list=filter_list, stop_lang=stop_lang, lang_modul=lang_modul) for chunk in chunks)
+    bar_list = [res for result in results for res in result]
+    #bar_list = [result[0] for result in results]
+    print(f'BAR LIST: {bar_list}')
+
+    return bar_list
+
+
 
 # Main
 def standard_pipeline(overview_file):
     df = read_csv(overview_file)
     
     # Standardize Dates
-    # df = standardize_dates(overview_file, df)
+    df = standardize_dates(overview_file, df)
 
     # language identification
-    # text_list = get_text_as_list(df, 'article_text')
-    text_list = get_text_as_list(df, 'context')
+    text_list = get_text_as_list(df, 'text')
+    #text_list = get_text_as_list(df, 'context')
 
-    # lang_id_list = detect_language(text_list, 'de') # ISO 639-1 codes for language ID; assign tag 'de' in case language is not detectable
-    # add_to_csv(df, 'language_id', lang_id_list, overview_file)
+    lang_id_list = detect_language(text_list, 'de') # ISO 639-1 codes for language ID; assign tag 'de' in case language is not detectable
+    add_to_csv(df, 'language_id', lang_id_list, overview_file)
+    lang_ids = get_text_as_list(df, 'language_id')
 
     # Lemmatization, lowercase, remove stopwords, keep only some POS Tags
-    lang_ids = get_text_as_list(df, 'language')
-
-    pos_tags_to_keep = ['NOUN', 'PROPN', 'VERB', 'INTERJ', 'PRON', 'ADJ', 'NUM'] # UPOS tagnames: https://universaldependencies.org/docs/u/pos/
-    filter_list = ["ftp", "http", "mailto", "www", "schweiz"]
+    pos_tags_to_keep = ['NOUN', 'PROPN']
+    #pos_tags_to_keep = ['NOUN', 'PROPN', 'VERB', 'INTERJ', 'PRON', 'ADJ'] # UPOS tagnames: https://universaldependencies.org/docs/u/pos/
+    filter_list = ["ftp", "http", "mailto", "www", "schweiz", "-"]
     
-    lemmatized_list = preprocess(text_list, lang_ids, pos_tags_to_keep, filter_list)
-    
-    add_to_csv(df, 'context_preprocessed', lemmatized_list, overview_file)
+    #lemmatized_list = preprocess(text_list, lang_ids, pos_tags_to_keep, filter_list)
+    lemmatized_list = preprocess(foo_list=text_list, language_list=lang_ids, pos_tags_to_keep=pos_tags_to_keep, filter_list=filter_list)
+    print(f'LEN lemmatized list: {len(lemmatized_list)}')
+    print(f'LEN text list: {len(text_list)}')
+    add_to_csv(df, 'text_preprocessed', lemmatized_list, overview_file)
 
-standard_pipeline('mediacorpus.csv')
-#standard_pipeline('corriere.csv')
+def main():
+    standard_pipeline('ALL_sgg_korpus_paragraphisiert_V1_radikaler.csv')
+    #standard_pipeline('corriere.csv')
+
+if __name__ == "__main__":
+    main()
